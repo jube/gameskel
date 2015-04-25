1820e6ed50c75a09a0346dfd5b0085140bf262a9
Julien Bernard <julien.bernard@univ-fcomte.fr>
2015-04-25 16:01:25 +0200

    update copyright year

--- src/template/game/Log.cc
+++ src/template/game/Log.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, Julien Bernard
+ * Copyright (c) 2014-2015, Julien Bernard
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
